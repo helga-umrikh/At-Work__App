01e63c0e94e43c1b62c107847a884270079e801b
helga-umrikh <zetaumr@gmail.com>
2024-04-06 22:22:41 +0300

    feat: add routing to HomePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage';
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path={'/'} element={<HomePage />}/>
+      </Routes>
     </div>
   );
 }
