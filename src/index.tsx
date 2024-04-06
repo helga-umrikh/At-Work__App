a3e488d3f2c42150aa734de89c84d0749ffddf8a
helga-umrikh <zetaumr@gmail.com>
2024-04-06 22:31:07 +0300

    feat: add styles for index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import './styles.scss'
+import ReactDOM from 'react-dom/client';
 import App from './App'
 import { BrowserRouter as Router } from 'react-router-dom'
 
