7abd1d8fd239914c17109f87706772054119baf0
helga-umrikh <zetaumr@gmail.com>
2024-04-11 13:46:12 +0300

    fix: route to home page, imports

--- src/components/ToHomePageComponent/ToHomePage.tsx
+++ src/components/ToHomePageComponent/ToHomePage.tsx
@@ -1,21 +1,20 @@
-import React from 'react';
-import './ToHomePage.scss';
-import { Link } from 'react-router-dom';
-import leftArrowIcon from '../../assets/arrow-left.svg'
+import React from 'react'
+import './ToHomePage.scss'
+import { Link } from 'react-router-dom'
 
 const ToHomePage = () => {
-  return (
-    <div className='to-home-page'>
-      <Link to={'/users'}>
-        <div className='to-home-page__container'>
-          <div className='to-home-page__arrow'>
-            <img src={leftArrowIcon} alt="left arrow" />
-          </div>
-          <p className='to-home-page__title'>Назад</p>
+    return (
+        <div className="to-home-page">
+            <Link to={'/'}>
+                <div className="to-home-page__container">
+                    <div className="to-home-page__arrow">
+                        <img src={'/assets/arrow-left.svg'} alt="left arrow" />
+                    </div>
+                    <p className="to-home-page__title">Назад</p>
+                </div>
+            </Link>
         </div>
-      </Link>
-    </div>
-  )
+    )
 }
 
 export default ToHomePage
