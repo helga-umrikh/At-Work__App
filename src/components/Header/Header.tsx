7c23d8b15f5bd82fb8d721b3a9113fe37bd5e9cc
helga-umrikh <zetaumr@gmail.com>
2024-04-12 17:01:46 +0300

    feat: change imports in Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,32 +1,37 @@
 import React from 'react'
 import './Header.scss'
-import logo from '../../assets/logo.svg'
-import likeIcon from '../../assets/like.svg'
-import notificationIcon from '../../assets/Notification.svg'
-import userPhoto from '../../assets/user__photo.png'
 
 const Header = () => {
     return (
         <div className="header">
             <div className="header__container">
                 <div className="header__logo">
-                    <img src={logo} alt="логотип и название компании At-work" />
+                    <img
+                        src={'/assets/logo.svg'}
+                        alt="логотип и название компании At-work"
+                    />
                 </div>
                 <div className="header__actions">
                     <button className="actions__likes" title="likes">
-                        <img src={likeIcon} alt="like icon" />
+                        <img src={'/assets/like.svg'} alt="like icon" />
                     </button>
                     <button
                         className="actions__notifications"
                         title="notifications"
                     >
-                        <img src={notificationIcon} alt="notification icon" />
+                        <img
+                            src={'/assets/Notification.svg'}
+                            alt="notification icon"
+                        />
                     </button>
                     <div className="header__user">
                         <div className="user__avatar">
-                            <img src={userPhoto} alt="user avatar" />
+                            <img
+                                src={'/assets/user__photo.png'}
+                                alt="user avatar"
+                            />
                         </div>
-                        <p className='user__name'>Ivan1234</p>
+                        <p className="user__name">Ivan1234</p>
                     </div>
                 </div>
             </div>
